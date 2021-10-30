93f6ec4b31b184df9fc17f245e9af79a5a0b9c33
dumanask <askimemir@gmail.com>
2021-10-30 23:04:20 +0300

    Dungeon

---
+++ Assets/Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    private void Start()
+    {
+        Attack();
+    }
+
+    public override void Attack()
+    {
+        
+    }
+    public override void Update()
+    {
+
+    }
+
+}
